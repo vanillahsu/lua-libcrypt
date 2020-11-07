5b2c333485c69c549cd595ccab640c4eacbd51e7
Vanilla Hsu <vanilla@fatpipi.com>
2020-11-07 22:08:05 +0800

    1: use thread-safe crypt(). 2: remove uneeded free(). 3: follow style(9) 4: convert hardcode case to enum.

--- lua_crypt.c
+++ lua_crypt.c
@@ -24,20 +24,29 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $FreeBSD: head/lib/flua/libjail/lua_jail.c 367013 2020-10-24 17:08:59Z freqlabs $
+ * $FreeBSD$
  */
 
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/lib/flua/libjail/lua_jail.c 367013 2020-10-24 17:08:59Z freqlabs $");
+__FBSDID("$FreeBSD$");
 
 #include <sys/param.h>
-#include <unistd.h>
+
+#include <lauxlib.h>
+#include <lua.h>
+#include <lualib.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
-#include <lua.h>
-#include <lauxlib.h>
-#include <lualib.h>
+enum {
+	CRYPT_DES = 0,
+	CRYPT_MD5,
+	CRYPT_BLOWFISH,
+	CRYPT_NTHASH,
+	CRYPT_SHA256,
+	CRYPT_SHA512
+};
 
 int luaopen_crypt(lua_State *);
 
@@ -45,25 +54,21 @@ static int
 lua_crypt(lua_State *L)
 {
 	const char *password, *hash;
-	struct crypt_data *buffer;
+	struct crypt_data data;
 	char *ret;
 
-	buffer = malloc(sizeof(*buffer));
-	if (buffer == NULL) {
-		return luaL_error(L, "malloc error");
-	}
-
-	memset(buffer, 0, sizeof(*buffer));
+	data.initialized = 0;
 	password = luaL_checkstring(L, 1);
 	hash = luaL_checkstring(L, 2);
 
-	ret = crypt_r(password, hash, buffer);
+	ret = crypt_r(password, hash, &data);
 	if (ret == NULL) {
-		return luaL_error(L, "fail to crypt");
+		lua_pushnil(L);
+		lua_pushstring(L, "fail to crypt");
+		return (2);
 	}
 
 	lua_pushstring(L, ret);
-	free(buffer);
 
 	return (1);
 }
@@ -74,30 +79,32 @@ lua_set_format(lua_State *L)
 	int ret, format;
 
 	format = luaL_checkinteger(L, 1);
-	switch(format) {
-		case 6:
-			ret = crypt_set_format("sha512");
-			break;
-		case 5:
-			ret = crypt_set_format("sha256");
-			break;
-		case 3:
-			ret = crypt_set_format("nth");
-			break;
-		case 2:
-			ret = crypt_set_format("blf");
-			break;
-		case 1:
-			ret = crypt_set_format("md5");
-			break;
-		default:
-		case 0:
-			ret = crypt_set_format("des");
+	switch (format) {
+	case CRYPT_SHA512:
+		ret = crypt_set_format("sha512");
+		break;
+	case CRYPT_SHA256:
+		ret = crypt_set_format("sha256");
+		break;
+	case CRYPT_NTHASH:
+		ret = crypt_set_format("nth");
+		break;
+	case CRYPT_BLOWFISH:
+		ret = crypt_set_format("blf");
+		break;
+	case CRYPT_MD5:
+		ret = crypt_set_format("md5");
+		break;
+	default:
+#ifdef HAS_DES
+	case CRYPT_DES:
+#endif
+		ret = crypt_set_format("des");
 	}
 
 	lua_pushinteger(L, ret);
 
-	return(1);
+	return (1);
 }
 
 static int
@@ -124,17 +131,17 @@ luaopen_crypt(lua_State *L)
 
 	luaL_setfuncs(L, l_crypt, 0);
 
-	lua_pushinteger(L, 0);
+	lua_pushinteger(L, CRYPT_DES);
 	lua_setfield(L, -2, "DES");
-	lua_pushinteger(L, 1);
+	lua_pushinteger(L, CRYPT_MD5);
 	lua_setfield(L, -2, "MD5");
-	lua_pushinteger(L, 2);
-	lua_setfield(L, -2, "BlowFish");
-	lua_pushinteger(L, 3);
-	lua_setfield(L, -2, "NTHash");
-	lua_pushinteger(L, 5);
+	lua_pushinteger(L, CRYPT_BLOWFISH);
+	lua_setfield(L, -2, "BLOWFISH");
+	lua_pushinteger(L, CRYPT_NTHASH);
+	lua_setfield(L, -2, "NTHASH");
+	lua_pushinteger(L, CRYPT_SHA256);
 	lua_setfield(L, -2, "SHA256");
-	lua_pushinteger(L, 6);
+	lua_pushinteger(L, CRYPT_SHA512);
 	lua_setfield(L, -2, "SHA512");
 
 	return (1);
